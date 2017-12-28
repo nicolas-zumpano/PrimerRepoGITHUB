3dab4b55c4c83b84b159040b3e4022bbfa449cff
Nicolas <nicozumpano@gmail.com>
2017-12-27 22:31:20 -0300

    Commit node_modules y demas

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/products', productRoutes);
+app.use('/products', orderRoutes);
+
+module.exports = app;
