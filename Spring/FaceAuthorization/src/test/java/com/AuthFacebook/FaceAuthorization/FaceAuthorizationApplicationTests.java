afd6fbfaf1e5bf853b9f1207cb0cca55bc6f1781
Nicolas <nicozumpano@gmail.com>
2017-12-27 23:50:38 -0300

    spring

---
+++ Spring/FaceAuthorization/src/test/java/com/AuthFacebook/FaceAuthorization/FaceAuthorizationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.AuthFacebook.FaceAuthorization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FaceAuthorizationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
